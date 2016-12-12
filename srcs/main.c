be7dd58e0456a35c5b1d61e19bd4eca1282dc55e
Paul Bie <pbie@student.42.fr>
2016-12-12 14:33:26 +0100

    Can now sort the files alphabetically and then display them.

--- srcs/main.c
+++ srcs/main.c
@@ -14,18 +14,21 @@
 
 //In this first version of the LS we get the current working directory
 //through the environment variable PWD. When we have the PWD we open it and
-//iterate through it's contents by reading the directory. Each file or folder
-//we encounter, we display it's name. Now we can color the file based on whether
+//iterate through it's contents by reading the directory. We then sort through
+//each file or folder to put them in alphabetical order. Once we have
+//alphabetized the folders we display them. The file colors are based on whether
 //it is an executable or a directory. We have also gotten rid of showing files
 //that begin with a period as a typical ls command without any options does
-//not show them. Have begun the alphabetizing.
+//not show them. Still need to diplay the output correctly on the screen.
+//NOTE: I have been taking pieces from the previous ft_ls I tried to make
+//6 months ago.
 
 int               main(int ac, char **av)
 {
   struct dirent   *dptr;
-  unsigned int    count;
-  unsigned int    j;
-  unsigned int    k;
+  int    count;
+  int    j;
+  int    k;
   t_main          m;
 
   count = 0;
@@ -49,12 +52,12 @@ int               main(int ac, char **av)
   while ((dptr = readdir(m.dp)) != NULL) {
     //Do not count the files beginning with '.'
     if (dptr->d_name[0] != '.') {
-      num_files++;
+      m.num_files++;
     }
   }
   //We are counting the number of files/folders inside the current working
   //directory. When it's done we close the directory.
-  closedir(dp);
+  closedir(m.dp);
 
   //We restore the values back to NULL since we will use them again later
 
@@ -64,29 +67,29 @@ int               main(int ac, char **av)
   //We then check that we should have at least one file/folder inside the
   //current working directory
 
-  if (!num_files) {
+  if (!m.num_files) {
     return(0);
   } else {
     //Allocate memory to hold the addresses of the names of contents in current
     //working directory
-    m.ptr = ft_malloc(num_files*8);
-    if (ptr == NULL) {
+    m.ptr = malloc(m.num_files*8);
+    if (m.ptr == NULL) {
       ft_putstr("Memory allocation failed");
       return(-1);
     } else {
       //Initialize the memory by zeros
-      memset(ptr, 0, num_files*8);
+      memset(m.ptr, 0, m.num_files*8);
     }
   }
 
   //Now we open the directory again
 
-  m.dp = opendir((const char*)curr_dir);
+  m.dp = opendir((const char*)m.curr_dir);
 
   if (m.dp == NULL) {
     ft_putstr("Error: Could not open the working directory");
     ft_putchar('\n');
-    free(ptr);
+    free(m.ptr);
     return(-1);
   }
   //ft_putchar('\n');
@@ -95,16 +98,16 @@ int               main(int ac, char **av)
   j = 0;
   while ((dptr = readdir(m.dp)) != NULL) {
     if (dptr->d_name[0] != '.') {
-      ptr[j] = (long)dptr->d_name;
+      m.ptr[j] = (long)dptr->d_name;
       j++;
     }
   }
   //Start sorting the names alphabetically using bubble sorting here
-  while (count < num_files - 1) {
+  while (count < m.num_files - 1) {
     k = count + 1;
-    while (k < (num_files)) {
-      char *c = (char*)ptr[count];
-      char *d = (char*)ptr[k];
+    while (k < (m.num_files)) {
+      char *c = (char*)m.ptr[count];
+      char *d = (char*)m.ptr[k];
       //Check that the two characters should be from the same set
       if ( ((*c >= 'a') && (*d >= 'a')) || ((*c <= 'Z') && (*d <= 'Z')) ) {
         int i = 0;
@@ -120,9 +123,9 @@ int               main(int ac, char **av)
         if (*(c + i) > *(d + i)) {
           //If yes, then swap the values
           long temp = 0;
-          temp = ptr[count];
-          ptr[count] = ptr[j];
-          ptr[j] = temp;
+          temp = m.ptr[count];
+          m.ptr[count] = m.ptr[j];
+          m.ptr[j] = temp;
         }
       } else {
         //If the two beginning characters are not from the same ASCII set then
@@ -148,18 +151,50 @@ int               main(int ac, char **av)
         if ((*c + off_1 + i) > (*d + off_2 + i)) {
           //If yes we do the swap
           long temp = 0;
-          temp = ptr[count];
-          ptr[count] = ptr[j];
-          ptr[j] = temp;
+          temp = m.ptr[count];
+          m.ptr[count] = m.ptr[j];
+          m.ptr[j] = temp;
         }
       }
       k++;
     }
     count++;
   }
+
+  //Now that the names are sorted alphabetically we need to display them
+  //to the console
+  count = 0;
+  while (count < m.num_files) {
+    //Check if the file/folder is executable
+    if (!access((const char*)m.ptr[count], X_OK)) {
+      int fd = -1;
+      struct stat st;
+
+      fd = open((char*)m.ptr[count], O_RDONLY, 0);
+      if (fd == -1) {
+        ft_putstr("Failed to open file/directory");
+        ft_putchar('\n');
+        free(m.ptr);
+        return(-1);
+      }
+      fstat(fd, &st);
+      if (S_ISDIR(st.st_mode)) {
+        //If folder, print in blue
+        ft_foldercolornorm(m.ptr, count);
+      } else {
+        //If executable print in green
+        ft_execcolornorm(m.ptr, count);
+      }
+      close(fd);
+    } else {
+      //If normal file print by default
+      ft_normcolornorm(m.ptr, count);
+    }
+    count++;
+  }
+  free(m.ptr);
   return(0);
 
 }
 
-//Will need to display the results in alphabetical order and then display them
-//in a better format.
+//Will need to display the results in a proper format
