b42c8ea3884ee75636dafe4297055043dfba4b27
Paul Bie <pbie@student.42.fr>
2017-06-18 17:05:26 +0200

    [Rework] - flag R now works with flag a properly

--- srcs/lg_r.c
+++ srcs/lg_r.c
@@ -24,76 +24,41 @@ void							ft_makeR(t_files *tmp, char *curr_dir, t_flags flags)
 	}
 }
 
-void							ft_printR(t_files *tmp, t_flags flags)
+void							ft_list_bis(t_files *tmp, char *curr_dir, t_flags flags)
 {
-	if (flags.l == TRUE)
-	{
-		ft_printpermissions((tmp)->stat);
-		ft_printinfo((tmp)->stat);
-		ft_printtime((tmp)->stat);
-	}
-	if (!access((const char*)(tmp)->dptr->d_name, X_OK)) //if you do it like this one
+	char						*newPath;
+
+	while (tmp->next)
 	{
 		if (S_ISDIR((tmp)->st_mode))
 		{
-			ft_foldercolorR((tmp)->name);
-			// ft_putendl("\n");
-			// if ((tmp)->dptr->d_name[0] != '.')
-			// if (ft_strcmp((tmp)->dptr->d_name, ".")
-			// 	&& ft_strcmp((tmp)->dptr->d_name, ".."))
-			// 	(tmp)->sub_dir = ft_list(make_path_fl(curr_dir, (tmp)->name), flags);
+			if (((tmp)->name[0] == '.'
+			&& ft_strcmp((tmp)->name, ".") != 0
+			&& ft_strcmp((tmp)->name, "..") != 0)
+			|| (tmp)->name[0] != '.')
+			{
+				newPath = make_path_fl(curr_dir, (tmp)->name);
+				ft_putchar('\n');
+				ft_putendl(newPath);
+				(tmp)->sub_dir = ft_list(newPath, flags);
+				free(tmp);
+			}
 		}
-		else
-			ft_execcolorR((tmp)->name);
+		tmp = tmp->next;
 	}
-	if (!access((const char*)(tmp)->dptr->d_name, X_OK))
-		ft_execcolorR((tmp)->name);
-	else
-		ft_putendl((tmp)->name);
 }
 
-// t_files						*ft_list(char *curr_dir, t_flags flags)
-// {
-// 	DIR						*ds;
-// 	struct dirent			*dptr;
-// 	t_files					*files;
-// 	t_files					*tmp;
-
-// 	files = NULL;
-// 	if(!(ds = opendir(curr_dir)))
-// 		return (NULL);
-// 	if(!(dptr = readdir(ds)))
-// 		return (NULL);
-// 	if(!flags.a)
-// 		while((dptr = readdir(ds)) && dptr->d_name[0] == '.')
-// 			ft_putchar('\0');
-// 	if(!(files = ft_listnew(dptr, curr_dir)))
-// 		return (NULL);
-// 	// ft_putchar('\n');
-// 	// ft_putendl(curr_dir);
-// 	while((dptr = readdir(ds)))
-// 		ft_lpb(&files, dptr, curr_dir);
-// 	// We do this because we are going to want to return the start of the list
-// 	// and if we iterate over it we will return the end of the list because we will
-// 	// have lost the head of the list address
-// 	tmp = files;
-// 	while (tmp->next)
-// 	{
-// 		ft_putendl((tmp)->name);
-// 		if (S_ISDIR((tmp)->st_mode)) //Problem is here
-// 		{
-// 				if (((tmp)->dptr->d_name[0] == '.'
-// 					&& strcmp((tmp)->dptr->d_name, ".") != 0
-// 					&& strcmp((tmp)->dptr->d_name, "..") != 0)
-// 					|| (tmp)->dptr->d_name[0] != '.')
-// 					(tmp)->sub_dir = ft_list(make_path_fl(curr_dir, (tmp)->name), flags);
-// 		}
-// 		tmp = tmp->next;
-// 	}
-// 	// ft_makeR(tmp, curr_dir, flags);
-// 	free(tmp);
-// 	return files;
-// }
+void							ft_printR(t_files *tmp)
+{
+	if (S_ISDIR((tmp)->st_mode))
+		ft_foldercolorR((tmp)->name);
+	else if (((tmp)->st_mode > 0) && (S_IEXEC & (tmp)->st_mode))
+		ft_execcolorR((tmp)->name);
+	else if (S_ISREG((tmp)->st_mode))
+		ft_putendl((tmp)->name);
+	else
+		ft_putchar('\0');
+}
 
 t_files						*ft_list(char *curr_dir, t_flags flags)
 {
@@ -125,41 +90,11 @@ t_files						*ft_list(char *curr_dir, t_flags flags)
 	tmp2 = files;
 	while (tmp->next)
 	{
-		if (S_ISDIR((tmp)->st_mode))
-			ft_foldercolorR((tmp)->name);
-		else if (((tmp)->st_mode > 0) && (S_IEXEC & (tmp)->st_mode))
-			ft_execcolorR((tmp)->name);
-		else if (S_ISREG((tmp)->st_mode))
-			ft_putendl((tmp)->name);
-		else
-			ft_putchar('\n');
+		ft_printR(tmp);
 		tmp = tmp->next;
 	}
-	if (S_ISDIR((tmp)->st_mode))
-		ft_foldercolorR((tmp)->name);
-	else if (((tmp)->st_mode > 0) && (S_IEXEC & (tmp)->st_mode))
-		ft_execcolorR((tmp)->name);
-	else if (S_ISREG((tmp)->st_mode))
-		ft_putendl((tmp)->name);
-	else
-		ft_putchar('\n');
-	while (tmp2->next)
-	{
-		if (S_ISDIR((tmp2)->st_mode))
-		{
-			if (((tmp2)->dptr->d_name[0] == '.'
-			&& strcmp((tmp2)->dptr->d_name, ".") != 0
-			&& strcmp((tmp2)->dptr->d_name, "..") != 0)
-			|| (tmp2)->dptr->d_name[0] != '.')
-			{
-				ft_putchar('\n');
-				ft_putendl(make_path_fl(curr_dir, (tmp2)->name));
-				(tmp2)->sub_dir = ft_list(make_path_fl(curr_dir, (tmp2)->name), flags);
-			}
-		}
-		tmp2 = tmp2->next;
-	}
-	
+	ft_printR(tmp);
+	ft_list_bis(tmp2, curr_dir, flags);
 	free(tmp);
 	return files;
 }
